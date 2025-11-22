e4ad790ad5316a81381064211996d3675a097a13
Hige6010 <3527250857@qq.com>
2025-11-22 16:48:55 +0800

    Enhance code readability with comments in EarlyStopping_hand.py
    Added comments in Chinese to clarify the purpose of variables and logic.

--- EarlyStopping_hand.py
+++ EarlyStopping_hand.py
@@ -15,19 +15,21 @@ class EarlyStopping:
         """
         self.patience = patience
         self.verbose = verbose
-        self.counter = 0
+        self.counter = 0# 计数器，用于记录验证损失连续没有改善的 epoch 数。
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.val_loss_min = np.Inf#历史最小验证损失
         self.delta = delta
 
     def __call__(self, val_loss, model, data_name, miss_rate, Name='TNet'):
 
-        score = val_loss
+        score = val_loss#将验证损失作为评估指标。
 
+        #if验证第一次调用
         if self.best_score is None:
             self.best_score = score
             self.save_checkpoint(val_loss, model, data_name, miss_rate, Name='TNet')
+        #如果成立，说明这一轮损失没有得到改善
         elif score < self.best_score + self.delta:
             self.counter += 1
             # print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -46,4 +48,5 @@ class EarlyStopping:
             pass
             # print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), f'/home/wangxiaoli/xiaoli/IMv_project/SaveModel/{data_name}/save_{Name}_{miss_rate}.pt')     # 这里会存储迄今最优模型的参数
+        #使用 PyTorch 的 torch.save 函数将模型的状态字典（state_dict）保存到 self.path 指定的文件中。state_dict 包含了模型所有层的权重和偏置。
         self.val_loss_min = val_loss
